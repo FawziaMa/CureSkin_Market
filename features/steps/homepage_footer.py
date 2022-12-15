7f966af458d3522ed6f71afa3504485de191264d
Fawzia Masud <fmasud11@gmail.com>
2022-12-14 19:41:46 -0500

    fmasud_CAP37

--- features/steps/homepage_footer.py
+++ features/steps/homepage_footer.py
@@ -4,8 +4,8 @@ from time import sleep
 
 
 @given('User is on homepage')
-def open_product_page(context):
-    context.app.homepage.open_main()
+def open_homepage(context):
+    context.app.homepage.open_homepage()
 
 
 @when('User scrolls down to footer')
@@ -20,7 +20,5 @@ def click_terms_of_service(context):
 
 @then('Verify {expected_title} page opened')
 def verify_terms_page(context, expected_title,):
-    context.homepage.verify_terms_page(expected_title)
-    # assert actual_results == expected_title
-
-    sleep(5)
+    actual_title = context.homepage.verify_terms_page(expected_title)
+    assert actual_title == expected_title
--- pages/homepage.py
+++ pages/homepage.py
@@ -5,13 +5,13 @@ from selenium.webdriver.support.ui import Select
 
 
 TERMS_OF_SERVICE = (By. XPATH, "//a[@href='/policies/terms-of-service']")
-TERMS_PAGE_TITLE = (By. XPATH, "//div[@class='shopify-policy__title']")
+TERMS_PAGE_TITLE = (By. XPATH, "//div[@class='shopify-policy__title']//h1['Terms of service']")
 
 
 class Homepage(Page):
 
-    def open_main(self):
-        self.driver.get('https://shop.cureskin.com/')
+    def open_homepage(self):
+        self.driver.get("https://shop.cureskin.com/")
 
     def scroll_to_footer(self):
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
@@ -19,6 +19,5 @@ class Homepage(Page):
     def click_terms_of_service(self):
         self.driver.find_element(*TERMS_OF_SERVICE).click()
 
-    def verify_terms_page(self, page_title):
-        title = Select(self.driver.find_element(*self.TERMS_PAGE_TITLE))
-        title.select_by_value(f"//h1{page_title}")
+    def verify_terms_page(self):
+        self.driver.find_element(*TERMS_PAGE_TITLE).text()
